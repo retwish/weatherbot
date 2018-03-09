83acb8953f1149bb1e53759e8283903fa2c8e61b
Anton <34218036+r1nko@users.noreply.github.com>
2018-03-09 14:17:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ import weather as weat
 vk = vk_api.VkApi(token = 'f53b3ee4912a5b03eb33589f74e9e979bc40fd532d527967c5fe3cbdd7a52620ab8c515983f6350ae9bfc')
 values = {'out': 0,'count': 100,'time_offset': 10}
 
-upload = vk_api.VkUpload(vk)
-photo  = upload.photo_messages('D:\Bot-Weather\weather.jpg')
-vk_photo_url = 'https://vk.com/photo{}_{}'.format(
-        photo[0]['owner_id'], photo[0]['id']
-        )
-a = vk_photo_url[15:-1]
-b, c = a.split('-')
-result = b + c
-
 
 def write_msg(user_id, s):
     vk.method('messages.send', {'user_id':user_id, 'attachment':'photo299887547_456242309', 'message':s})
